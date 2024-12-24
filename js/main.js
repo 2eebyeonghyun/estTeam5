5ee41426e89df096d4fc8ae165e68354c21aa33d
2eebyeonghyun <wada0012@naver.com>
2024-12-24 10:01:12 +0900

    section07 비디오 영역 자바스크립트문 추가 작업

--- js/main.js
+++ js/main.js
@@ -14,4 +14,32 @@ function menuBtn(){
     });
 }
 
+function videoPlay(){
+    const video = document.querySelector('.videoBox__video');
+    const videoBtn = document.querySelector('.videoBox__btn');
+    const videoIcon = document.querySelector('.videoBox__btn .btn-play');
+
+    videoIcon.innerHTML = `<i class="fa-solid fa-play"></i>`;
+
+    videoBtn.addEventListener('click', function(){
+
+        // 만약 videoBtn에 play클래스가 존재하면
+        // 버튼 클릭 시 비디오를 재생시킨다.
+        // 또, play 클래스를 삭제하고 pause 클래스를 추가한다.
+        // 그리고 아이콘영역에 폰트어썸아이콘을 가져온다.
+        if(videoBtn.classList.contains('play')){
+            video.play();
+            videoIcon.innerHTML = `<i class="fa-solid fa-pause"></i>`;
+            videoBtn.classList.remove('play');
+            videoBtn.classList.add('pause');
+        } else if (videoBtn.classList.contains('pause')) {
+            video.pause();
+            videoIcon.innerHTML = `<i class="fa-solid fa-play"></i>`;
+            videoBtn.classList.remove('pause');
+            videoBtn.classList.add('play');
+        }
+    });
+}
+
 menuBtn();
+videoPlay();
